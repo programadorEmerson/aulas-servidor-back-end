c0c1510e4babc91c940def3a6440631632e9e370
Emerson Saturnino <programador.emerson@hotmail.com>
2021-09-18 15:56:15 -0300

    Autenticação JWT

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,4 +1,6 @@
 import { Router } from 'express';
+import VerififyToken from '../middleware/usuarios.middleware';
+import { requestLogin } from '../models/usuario.model';
 import {
   getAll, createUser, deleteUser, updateUser,
 } from '../controllers/usuario.controller';
@@ -9,12 +11,14 @@ routes.get('/', (req, res) => {
   res.status(200).json({ ok: 'conected' });
 });
 
-routes.get('/usuario', getAll);
+routes.get('/usuario', VerififyToken, getAll);
 
-routes.post('/usuario', createUser);
+routes.get('/login', requestLogin);
 
-routes.delete('/usuario/:id', deleteUser);
+routes.post('/usuario', VerififyToken, createUser);
 
-routes.put('/usuario/:id', updateUser);
+routes.delete('/usuario/:id', VerififyToken, deleteUser);
+
+routes.put('/usuario/:id', VerififyToken, updateUser);
 
 export default routes;
